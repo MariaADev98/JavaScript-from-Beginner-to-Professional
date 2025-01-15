63667ef5d9e41f8c79d62e44a00e3568b550e617
MariaADev98 <bulatova2798@gmail.com>
2025-01-15 19:21:44 +0300

    Commit 15_01_2025

---
+++ Chapter 2/Pr_Le_2.2.js
@@ -0,0 +1,5 @@
+const myname = 'Maria';
+const age = 26;
+const coder = true;
+const message = 'Hello, my name is ' + myname + ', I am ' + age + ' years old and I can code JS: ' + coder + '.'
+console.log(message);
