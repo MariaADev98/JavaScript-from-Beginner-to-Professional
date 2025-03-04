675e3cdd59fbd226933304f466df72dc02e37e94
MariaADev98 <bulatova2798@gmail.com>
2025-03-04 17:46:21 +0300

    04.03

---
+++ Chapter 7/Pr_Le_7.1.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+    };
+}
+    let oldFriend = new Person('Vas', 'Cud');
+    let friend = new Person('Jul', 'Tar');
+    console.log('Hello' + oldFriend)
+    console.log('Hello' + friend)
+
